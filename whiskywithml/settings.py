f29d06f2e77ca257f2880bc0ffe63f11a322e32f
alokknight <alokpatel885@gmail.com>
2022-02-09 13:01:42 +0530

    secrect key removed

--- whiskywithml/settings.py
+++ whiskywithml/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a1^kqkv%fhg!muj1#%8&8k-e*palnj-e-26bu!7&3l4wvt+p$-'
+SECRET_KEY = 'your secret key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
